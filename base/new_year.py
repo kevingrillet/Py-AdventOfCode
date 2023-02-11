7225b26525efa87aecb58b79d430b544bfaf65ca
kevingrillet <kevin.grillet@gmail.com>
2023-02-11 15:02:49 +0100

    Update new_year.py
    - Optimization
    - Update main.py if one line inpt

--- base/new_year.py
+++ base/new_year.py
@@ -24,8 +24,14 @@ def add_day_input(year: str, d: int) -> None:
     url = f'https://adventofcode.com/{year}/day/{d}'
     rq = requests.get(f'{url}/input', cookies=COOKIES)
     if rq.status_code == 200:
-        inpt = open(f'../{year}/{day}/input', 'w')
+        inpt = open(f'../{year}/{day}/input', 'w+')
         inpt.write(rq.text)
+        inpt.seek(0)
+        if len(inpt.readlines()) == 1:
+            replace_in_file([f'../{year}/{day}/main.py'], [('list[str]', 'str'),
+                                                           ('# return f.read().strip()', 'return f.read().strip()')])
+            remove_line_in_file([f'../{year}/{day}/main.py'],
+                                [r'return \[line\.strip\(\) for line in f\.readlines\(\)\]'])
         inpt.close()
     else:
         raise NameError(f'requests.get({url}/input, cookies=COOKIES) responded with a status of: {rq.status_code}.')
@@ -45,17 +51,20 @@ def add_title_readme(year: str) -> None:
                 days.append(int(find.group(1)))
 
     if len(days) > 0:
+        days_to_add = []
         for day in days:
             url = f'https://adventofcode.com/{year}/day/{day}'
             rq = requests.get(url)
             if rq.status_code == 200:
                 soup = BeautifulSoup(rq.content, 'html.parser')
                 title = ' '.join(soup.find('h2').get_text().split()[3:-1])
-                replace_in_file(f'../{year}/README.md', f'[](<https://adventofcode.com/{year}/day/{day}>)',
-                                f'[{title}](<https://adventofcode.com/{year}/day/{day}>)')
+                days_to_add.append((f'[](<https://adventofcode.com/{year}/day/{day}>)',
+                                    f'[{title}](<https://adventofcode.com/{year}/day/{day}>)'))
             else:
                 print(f'requests.get({url}) responded with a status of: {rq.status_code}.')
                 break
+        if len(days_to_add) > 0:
+            replace_in_file([f'../{year}/README.md'], days_to_add)
 
 
 def create_new_day(year: str, d: int) -> None:
@@ -80,6 +89,7 @@ def create_new_days(year: str) -> None:
     """
     for d in range(1, 26):
         create_new_day(year, d)
+        print(f'Done: Year {year} Day {"{:02d}".format(d)}')
 
 
 def create_new_year(year: str) -> None:
@@ -93,31 +103,53 @@ def create_new_year(year: str) -> None:
         shutil.copytree('./year/', f'../{year}')
 
     # Set year in files
-    replace_in_file(f'../{year}/README.md', 'XXXX', year)
-    replace_in_file(f'../{year}/main.py', 'XXXX', year)
+    replace_in_file([f'../{year}/README.md', f'../{year}/main.py'], [('XXXX', year)])
 
     add_title_readme(year)
+    print(f'Done: Year {year}')
 
 
-def replace_in_file(path: str, match: str, replace: str) -> None:
+def remove_line_in_file(paths: list[str], matchs: list[str]) -> None:
     """
-    Replace match with replace in path file.
-    :param path: path of the file
-    :param match: match value
-    :param replace: replace value
+    Remove line of match in path file.
+    :param matchs: list of match
+    :param paths: list of paths of the files
     :return:
     """
-    file = open(path, 'r')
-    filedata = file.read()
-    file.close()
+    for path in paths:
+        file = open(path, 'r')
+        filedata = file.read()
+        file.close()
 
-    newdata = filedata.replace(match, replace)
+        for match in matchs:
+            filedata = re.sub(f'.*{match}\n', '', filedata)
 
-    file = open(path, 'w')
-    file.write(newdata)
-    file.close()
+        file = open(path, 'w')
+        file.write(filedata)
+        file.close()
+
+
+def replace_in_file(paths: list[str], matchs: list[tuple[str, str]]) -> None:
+    """
+    Replace match with replace in path file.
+    :param matchs: list of (match, replace)
+    :param paths: list of paths of the files
+    :return:
+    """
+    for path in paths:
+        file = open(path, 'r')
+        filedata = file.read()
+        file.close()
+
+        for match in matchs:
+            filedata = filedata.replace(match[0], match[1])
+
+        file = open(path, 'w')
+        file.write(filedata)
+        file.close()
 
 
 if __name__ == '__main__':
     create_new_year(NEW)
     create_new_days(NEW)
+    print(f'Done!')
